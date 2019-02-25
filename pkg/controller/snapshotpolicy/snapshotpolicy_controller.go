41340f5dfa98ccb22762f5df058ef36a1c720d16
John Strunk <jstrunk@redhat.com>
2019-02-25 18:18:02 -0500

    Switch from SnapshotSchedule to SnapshotPolicy

--- pkg/controller/snapshotpolicy/snapshotpolicy_controller.go
+++ pkg/controller/snapshotpolicy/snapshotpolicy_controller.go
@@ -1,4 +1,4 @@
-package snapshotschedule
+package snapshotpolicy
 
 import (
 	"context"
@@ -20,14 +20,14 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-var log = logf.Log.WithName("controller_snapshotschedule")
+var log = logf.Log.WithName("controller_snapshotpolicy")
 
 /**
 * USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
 * business logic.  Delete these comments after modifying this file.*
  */
 
-// Add creates a new SnapshotSchedule Controller and adds it to the Manager. The Manager will set fields on the Controller
+// Add creates a new SnapshotPolicy Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
@@ -35,28 +35,28 @@ func Add(mgr manager.Manager) error {
 
 // newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileSnapshotSchedule{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+	return &ReconcileSnapshotPolicy{client: mgr.GetClient(), scheme: mgr.GetScheme()}
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Create a new controller
-	c, err := controller.New("snapshotschedule-controller", mgr, controller.Options{Reconciler: r})
+	c, err := controller.New("snapshotpolicy-controller", mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
 
-	// Watch for changes to primary resource SnapshotSchedule
-	err = c.Watch(&source.Kind{Type: &snapschedulerv1alpha1.SnapshotSchedule{}}, &handler.EnqueueRequestForObject{})
+	// Watch for changes to primary resource SnapshotPolicy
+	err = c.Watch(&source.Kind{Type: &snapschedulerv1alpha1.SnapshotPolicy{}}, &handler.EnqueueRequestForObject{})
 	if err != nil {
 		return err
 	}
 
 	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner SnapshotSchedule
+	// Watch for changes to secondary resource Pods and requeue the owner SnapshotPolicy
 	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
 		IsController: true,
-		OwnerType:    &snapschedulerv1alpha1.SnapshotSchedule{},
+		OwnerType:    &snapschedulerv1alpha1.SnapshotPolicy{},
 	})
 	if err != nil {
 		return err
@@ -65,29 +65,29 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	return nil
 }
 
-var _ reconcile.Reconciler = &ReconcileSnapshotSchedule{}
+var _ reconcile.Reconciler = &ReconcileSnapshotPolicy{}
 
-// ReconcileSnapshotSchedule reconciles a SnapshotSchedule object
-type ReconcileSnapshotSchedule struct {
+// ReconcileSnapshotPolicy reconciles a SnapshotPolicy object
+type ReconcileSnapshotPolicy struct {
 	// This client, initialized using mgr.Client() above, is a split client
 	// that reads objects from the cache and writes to the apiserver
 	client client.Client
 	scheme *runtime.Scheme
 }
 
-// Reconcile reads that state of the cluster for a SnapshotSchedule object and makes changes based on the state read
-// and what is in the SnapshotSchedule.Spec
+// Reconcile reads that state of the cluster for a SnapshotPolicy object and makes changes based on the state read
+// and what is in the SnapshotPolicy.Spec
 // TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
 // a Pod as an example
 // Note:
 // The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
-func (r *ReconcileSnapshotSchedule) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+func (r *ReconcileSnapshotPolicy) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
-	reqLogger.Info("Reconciling SnapshotSchedule")
+	reqLogger.Info("Reconciling SnapshotPolicy")
 
-	// Fetch the SnapshotSchedule instance
-	instance := &snapschedulerv1alpha1.SnapshotSchedule{}
+	// Fetch the SnapshotPolicy instance
+	instance := &snapschedulerv1alpha1.SnapshotPolicy{}
 	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
@@ -103,7 +103,7 @@ func (r *ReconcileSnapshotSchedule) Reconcile(request reconcile.Request) (reconc
 	// Define a new Pod object
 	pod := newPodForCR(instance)
 
-	// Set SnapshotSchedule instance as the owner and controller
+	// Set SnapshotPolicy instance as the owner and controller
 	if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
 		return reconcile.Result{}, err
 	}
@@ -130,7 +130,7 @@ func (r *ReconcileSnapshotSchedule) Reconcile(request reconcile.Request) (reconc
 }
 
 // newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *snapschedulerv1alpha1.SnapshotSchedule) *corev1.Pod {
+func newPodForCR(cr *snapschedulerv1alpha1.SnapshotPolicy) *corev1.Pod {
 	labels := map[string]string{
 		"app": cr.Name,
 	}
