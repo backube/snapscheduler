1cc039d6c0c91ee8d3d96526480f3a0d558c5eef
John Strunk <jstrunk@redhat.com>
2020-01-31 15:20:24 -0500

    Enhance test to ensure snapshot becomes ready
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- .ci-scripts/tests/test-simple-schedule.sh
+++ .ci-scripts/tests/test-simple-schedule.sh
@@ -2,6 +2,20 @@
 
 set -e -o pipefail
 
+function fail_no_snaps {
+    echo "Timeout waiting. No snapshots were created."
+    kubectl describe pvc/pvc
+    kubectl get snapshotschedule/minute -oyaml
+    kubectl -n backube-snapscheduler logs -lapp.kubernetes.io/name=snapscheduler
+}
+
+function fail_snap_not_ready {
+    echo "Timeout waiting. Snapshot did not become ready"
+    echo "Snap name: $1"
+    kubectl get "volumesnapshot/$1" -oyaml
+    kubectl describe "volumesnapshot/$1"
+}
+
 # Create a PVC/PV
 cat - <<PVC | kubectl create -f -
 ---
@@ -27,7 +41,7 @@ metadata:
   name: minute
 spec:
   retention:
-    maxCount: 2
+    maxCount: 5
   schedule: "* * * * *"
   snapshotTemplate:
     snapshotClassName: csi-hostpath-snapclass
@@ -37,10 +51,24 @@ SCHED
 DEADLINE=$(( SECONDS + 90 ))
 while [[ $(kubectl get volumesnapshots 2> /dev/null | wc -l) -lt 1 ]]; do
     echo "Waiting for snapshots..."
-    [[ $SECONDS -lt $DEADLINE ]] || ( echo "Timeout waiting. No snapshots were created." && exit 1 )
+    [[ $SECONDS -lt $DEADLINE ]] || ( fail_no_snaps && exit 1 )
     sleep 10
 done
 
+# Get the name of the snapshot
+DEADLINE=$(( SECONDS + 120 ))
+SNAP_NAME=$(kubectl get volumesnapshots --no-headers | awk '{ print $1 }')
+echo "Found snapshot: $SNAP_NAME"
+
+# Wait for it to become "ready"
+while [[ $(kubectl get "volumesnapshot/$SNAP_NAME" -ojsonpath="{.status.readyToUse}") != "true" ]]; do
+  echo "Waiting for snapshot to be ready..."
+    [[ $SECONDS -lt $DEADLINE ]] || ( fail_snap_not_ready "$SNAP_NAME" && exit 1 )
+    sleep 10
+done
+echo "Snapshot $SNAP_NAME is ready!"
+
+# Clean up
 kubectl delete SnapshotSchedule/minute
 kubectl delete volumesnapshots -l 'snapscheduler.backube/schedule=minute'
 kubectl delete pvc/pvc
