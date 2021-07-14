2d0cd79dc8737815a6cd67cb077f76e3f2a22a54
John Strunk <jstrunk@redhat.com>
2021-07-14 12:15:59 -0400

    Use custom tag in local kind cluster
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- hack/run-in-kind.sh
+++ hack/run-in-kind.sh
@@ -2,9 +2,17 @@
 
 set -e -o pipefail
 
+# cd to top dir
+scriptdir="$(dirname "$(realpath "$0")")"
+cd "$scriptdir/.."
+
 make docker-build
 
-kind load docker-image "quay.io/backube/snapscheduler"
+KIND_TAG=local-build
+IMAGE="quay.io/backube/snapscheduler"
+
+docker tag "${IMAGE}:latest" "${IMAGE}:${KIND_TAG}"
+kind load docker-image "${IMAGE}:${KIND_TAG}"
 
 kubectl create ns backube-snapscheduler
-helm install -n backube-snapscheduler --set image.tagOverride=latest snapscheduler ./helm/snapscheduler
+helm install -n backube-snapscheduler --set image.tagOverride=${KIND_TAG} snapscheduler ./helm/snapscheduler
